f0e11755e0c37a538f48b805c5067d7238e2ba1a
fortuna <fortuna@localhost>
2010-01-16 02:15:55 +0000

    Added status bar

--- src/main/groovy/org/mnode/coucou/Coucou.groovy
+++ src/main/groovy/org/mnode/coucou/Coucou.groovy
@@ -1,5 +1,6 @@
 /**
- * This file is part of Base Modules.
+
+* This file is part of Base Modules.
  *
  * Copyright (c) 2010, Ben Fortuna [fortuna@micronode.com]
  *
@@ -18,12 +19,46 @@
  */
 package org.mnode.coucou
 
-import groovy.swing.SwingXBuilderimport java.awt.Insetsimport javax.swing.UIManagerimport groovy.swing.LookAndFeelHelperimport java.awt.TrayIconimport java.awt.PopupMenuimport java.awt.SystemTrayimport java.awt.MenuItemimport javax.swing.JFrame
+import groovy.swing.SwingXBuilder
+import java.awt.BorderLayout
+import java.awt.Insets
+import java.awt.event.MouseEvent
+import javax.swing.UIManager
+import groovy.swing.LookAndFeelHelper
+import java.awt.TrayIcon
+import java.awt.PopupMenu
+import java.awt.SystemTray
+import java.awt.MenuItem
+import javax.swing.JFrame
+import javax.swing.JScrollPane
+import javax.swing.JTabbedPane
+import org.jvnet.substance.SubstanceLookAndFeel
+import org.jvnet.substance.api.SubstanceConstants
+import org.jvnet.substance.api.SubstanceConstants.TabCloseKind
+import org.jvnet.substance.api.tabbed.TabCloseCallback
+import org.jvnet.lafwidget.LafWidget
+import org.jvnet.lafwidget.tabbed.DefaultTabPreviewPainter
 
 /**
  * @author fortuna
  *
  */
+ /*
+@Grapes([
+    @Grab(group='org.codehaus.griffon.swingxbuilder', module='swingxbuilder', version='0.1.6'),
+    @Grab(group='net.java.dev.substance', module='substance', version='5.3'),
+    @Grab(group='net.java.dev.substance', module='substance-swingx', version='5.3'),
+    //@Grab(group='org.swinglabs', module='swingx', version='0.9.2'),
+    @Grab(group='org.mnode.base', module='base-views', version='0.0.1-SNAPSHOT'),
+    //@Grab(group='jgoodies', module='forms', version='1.0.5'),
+    //@Grab(group='org.codehaus.griffon.flamingobuilder', module='flamingobuilder', version='0.2'),
+    @Grab(group='net.java.dev.flamingo', module='flamingo', version='4.2'),
+    @Grab(group='org.apache.xmlgraphics', module='batik-awt-util', version='1.7'),
+    @Grab(group='org.apache.xmlgraphics', module='batik-swing', version='1.7'),
+    @Grab(group='org.apache.xmlgraphics', module='batik-transcoder', version='1.7'),
+    @Grab(group='net.java.dev.datatips', module='datatips', version='20091219'),
+    @Grab(group='com.fifesoft.rsyntaxtextarea', module='rsyntaxtextarea', version='1.4.0')])
+    */
 public class Coucou{
      
      static void close(def frame, def exit) {
@@ -49,21 +84,74 @@ public class Coucou{
 
              frame(title: 'Coucou', id: 'coucouFrame', defaultCloseOperation: JFrame.DO_NOTHING_ON_CLOSE,
                      size: [800, 600], show: false, locationRelativeTo: null, iconImage: imageIcon('/logo.png', id: 'logoIcon').image) {
+                
+                actions() {
+                    action(id: 'busyAction', name: 'Busy', smallIcon: imageIcon('/busy.png'), closure: {})
+                    action(id: 'invisibleAction', name: 'Invisible', smallIcon: imageIcon('/invisible.png'), closure: {})
+                    action(id: 'workOfflineAction', name: 'Work Offline', smallIcon: imageIcon('/offline.png'), accelerator: shortcut('shift O'))
+                }
+                
+                borderLayout()
                  
-                 borderLayout()
-                 splitPane(id: 'splitPane', oneTouchExpandable: true, dividerLocation: 1.0) {
-                     panel(constraints: 'left', border: emptyBorder(10)) {
+                tabbedPane(tabLayoutPolicy: JTabbedPane.SCROLL_TAB_LAYOUT, id: 'tabs') {
+                    panel(name: 'Home') {
                          borderLayout()
-                         scrollPane(border: null) {
-                             list()
+                         splitPane(id: 'splitPane', oneTouchExpandable: true, dividerLocation: 1.0) {
+                             panel(constraints: 'left', border: emptyBorder(10)) {
+                                 borderLayout()
+                                 scrollPane(horizontalScrollBarPolicy: JScrollPane.HORIZONTAL_SCROLLBAR_NEVER, border: null) {
+                                     list()
+                                 }
+                             }
+                             panel(constraints: 'right', border: emptyBorder(10))
                          }
                      }
-                     panel(constraints: 'right', border: emptyBorder(10))
+                 }
+                 tabs.putClientProperty(SubstanceLookAndFeel.TABBED_PANE_CONTENT_BORDER_KIND, SubstanceConstants.TabContentPaneBorderKind.SINGLE_FULL)
+                 tabs.putClientProperty(SubstanceLookAndFeel.TABBED_PANE_CLOSE_CALLBACK, new TabCloseCallbackImpl())
+                 tabs.putClientProperty(LafWidget.TABBED_PANE_PREVIEW_PAINTER, new DefaultTabPreviewPainter())
+             
+                 statusBar(constraints: BorderLayout.SOUTH, border:emptyBorder([0, 4, 0, 4]), id: 'cStatusBar') {
+                     label(id: 'statusMessage', text: 'Ready')
+                    toggleButton(busyAction,
+                            text: null,
+                            toolTipText: 'Busy',
+                            selectedIcon: imageIcon('/busy_selected.png'),
+                            rolloverIcon: imageIcon('/busy_rollover.png'),
+                            margin: null,
+                            border: emptyBorder([0, 4, 0, 4]),
+                            borderPainted: false,
+                            contentAreaFilled: false,
+                            focusPainted: false,
+                            opaque: false)
+    //                        contraints: statusBarContraints)
+                    toggleButton(invisibleAction,
+                            text: null,
+                            toolTipText: 'Invisible',
+                            selectedIcon: imageIcon('/invisible_selected.png'),
+                            rolloverIcon: imageIcon('/invisible_rollover.png'),
+                            margin: null,
+                            border: emptyBorder([0, 4, 0, 4]),
+                            borderPainted: false,
+                            contentAreaFilled: false,
+                            focusPainted: false,
+                            opaque: false)
+                    toggleButton(workOfflineAction,
+                            text: null,
+                            toolTipText: 'Work Offline',
+                            selectedIcon: imageIcon('/offline_selected.png'),
+                            rolloverIcon: imageIcon('/offline_rollover.png'),
+                            margin: null,
+                            border: emptyBorder([0, 4, 0, 4]),
+                            borderPainted: false,
+                            contentAreaFilled: false,
+                            focusPainted: false,
+                            opaque: false)
                  }
              }
-             
+
              if (SystemTray.isSupported()) {
-                 TrayIcon trayIcon = new TrayIcon(logoIcon.image, 'Coucou')
+                 TrayIcon trayIcon = new TrayIcon(imageIcon('/logo-12.png').image, 'Coucou')
                  trayIcon.imageAutoSize = false
                  trayIcon.mousePressed = { event ->
                      if (event.button == MouseEvent.BUTTON1) {
@@ -99,4 +187,46 @@ public class Coucou{
          
      }
     
+}
+
+
+class TabCloseCallbackImpl implements TabCloseCallback {
+
+      public TabCloseKind onAreaClick(JTabbedPane tabbedPane, int tabIndex, MouseEvent mouseEvent) {
+        if (mouseEvent.getButton() != MouseEvent.BUTTON2)
+          return TabCloseKind.NONE;
+        if (mouseEvent.isShiftDown()) {
+          return TabCloseKind.ALL;
+        }
+        return TabCloseKind.THIS;
+      }
+
+      public TabCloseKind onCloseButtonClick(JTabbedPane tabbedPane,
+          int tabIndex, MouseEvent mouseEvent) {
+        if (mouseEvent.isAltDown()) {
+          return TabCloseKind.ALL_BUT_THIS;
+        }
+        if (mouseEvent.isShiftDown()) {
+          return TabCloseKind.ALL;
+        }
+        return TabCloseKind.THIS;
+      }
+
+      public String getAreaTooltip(JTabbedPane tabbedPane, int tabIndex) {
+        return null;
+      }
+
+      public String getCloseButtonTooltip(JTabbedPane tabbedPane,
+          int tabIndex) {
+        StringBuffer result = new StringBuffer();
+        result.append("<html><body>");
+        result.append("Mouse click closes <b>"
+            + tabbedPane.getTitleAt(tabIndex) + "</b> tab");
+        result
+            .append("<br><b>Alt</b>-Mouse click closes all tabs but <b>"
+                + tabbedPane.getTitleAt(tabIndex) + "</b> tab");
+        result.append("<br><b>Shift</b>-Mouse click closes all tabs");
+        result.append("</body></html>");
+        return result.toString();
+      }
 }
