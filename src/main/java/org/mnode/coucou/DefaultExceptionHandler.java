26f60b23b22bf63f3e50a656ebb1325129abbc54
benfortuna <benfortuna@gmail.com>
2011-01-23 18:31:15 +1100

    Implemented default exception handler

---
+++ src/main/java/org/mnode/coucou/DefaultExceptionHandler.java
@@ -0,0 +1,49 @@
+package org.mnode.coucou;
+
+import java.awt.Component;
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import org.jdesktop.swingx.JXErrorPane;
+import org.jdesktop.swingx.error.ErrorInfo;
+import org.mnode.base.log.FormattedLogEntry;
+import org.mnode.base.log.LogAdapter;
+import org.mnode.base.log.LogEntry;
+import org.mnode.base.log.LogEntry.Level;
+import org.mnode.base.log.adapter.Slf4jAdapter;
+import org.slf4j.LoggerFactory;
+
+public class DefaultExceptionHandler implements UncaughtExceptionHandler {
+	
+	private static final LogAdapter LOG = new Slf4jAdapter(LoggerFactory.getLogger(UncaughtExceptionHandler.class));
+	
+	private static final LogEntry UNEXPECTED_ERROR = new FormattedLogEntry(Level.Error,
+			"An unexpected error has occurred in thread %s");
+
+	private Component dialogOwner;
+	
+	@Override
+	public void uncaughtException(Thread thread, Throwable exception) {
+		LOG.log(UNEXPECTED_ERROR, thread, exception);
+		
+		final ErrorInfo error = new ErrorInfo("Error", exception.getMessage(),
+				String.format("<html><body>Unexpected error in thead <em>%s</em>: %s</body></html>", thread, exception),
+				null, null, null, null);
+		
+		JXErrorPane.showDialog(dialogOwner, error);
+	}
+
+	/**
+	 * @return the dialogOwner
+	 */
+	public Component getDialogOwner() {
+		return dialogOwner;
+	}
+
+	/**
+	 * @param dialogOwner the dialogOwner to set
+	 */
+	public void setDialogOwner(Component dialogOwner) {
+		this.dialogOwner = dialogOwner;
+	}
+
+}
