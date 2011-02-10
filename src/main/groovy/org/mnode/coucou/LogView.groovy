02903c0b692b2a885d38968fc6bad17b376c9086
benfortuna <benfortuna@gmail.com>
2011-02-10 20:28:24 +1100

    Added license header

--- src/main/groovy/org/mnode/coucou/LogView.groovy
+++ src/main/groovy/org/mnode/coucou/LogView.groovy
@@ -1,3 +1,21 @@
+/**
+ * This file is part of Coucou.
+ *
+ * Copyright (c) 2010, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Coucou is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Coucou is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Coucou.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.mnode.coucou
 
 import org.apache.commons.io.input.Tailer;
